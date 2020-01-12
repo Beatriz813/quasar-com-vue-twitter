8d418b521835e8a1f300a8f6e7b0e2e86cce1604
Beatriz813 <beatrizalcantara813@gmail.com>
2020-01-12 13:42:24 -0300

    Primeiro commit

---
+++ src/boot/twitter.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import twitter from 'vue-twitter'
+
+Vue.use(twitter)
---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="container">
+    <div class="row">
+      <div class="col-3">
+      <twitter>
+        <a class="twitter-timeline" href="https://twitter.com/rla4?ref_src=twsrc%5Etfw">Tweets by rla4</a>
+      </twitter>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
